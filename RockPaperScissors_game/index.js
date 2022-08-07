9ebaa2f84013f6cc3559bae2406e02b4278984f4
leesoyuun <leesoyun4206@naver.com>
2022-08-07 19:13:14 +0900

    add score function

--- RockPaperScissors_game/index.js
+++ RockPaperScissors_game/index.js
@@ -4,8 +4,13 @@ const scissorsBtn = document.getElementById("scissors").addEventListener('click'
 const paperBtn = document.getElementById("paper").addEventListener('click',paperclick);
 const rockBtn = document.getElementById("rock").addEventListener('click',rockclick);
 const reBtn = document.getElementById("re").addEventListener('click',Re);
+const computerScore = document.getElementById("ComScore")
+const PeopleScore = document.getElementById("PeopleScore")
 
 let index = 0;
+let comScore = 0;
+let PepScore = 0;
+
 function Random() {
     imogeShowin.innerText = `${imogeShow[index++]}`;
     if(index == imogeShow.length){
@@ -13,7 +18,7 @@ function Random() {
     }
 }   
 
-let randomImoge = setInterval(Random,1000);
+let randomImoge = setInterval(Random,100);
 
 
 function click() {
@@ -26,9 +31,11 @@ function scissorclick(){
     }
     else if(imogeShowin.innerText == "✊" ){
         alert("졌다.");
+        computerScore.innerText = `${++comScore}`;
     }
     else{
-        alert("이겼다.")
+        alert("이겼다.");
+        PeopleScore.innerText = `${++PepScore}`;
         
     }
     
@@ -38,12 +45,14 @@ function paperclick(){
     click();
     if(imogeShowin.innerText == "✌"){
         alert("졌다.");
+        computerScore.innerText = `${++comScore}`;
     }
     else if(imogeShowin.innerText == "✊" ){
         alert("이겼다.");
+        PeopleScore.innerText = `${++PepScore}`;
     }
     else{
-        alert("비겼다.")
+        alert("비겼다. 다시!")
     }
     
 }
@@ -51,16 +60,18 @@ function rockclick(){
     click();
     if(imogeShowin.innerText == "✌"){
         alert("이겼다.");
+        PeopleScore.innerText = `${++PepScore}`;
     }
     else if(imogeShowin.innerText == "✊" ){
-        alert("비겼다.");
+        alert("비겼다. 다시!");
     }
     else{
-        alert("졌다.")
+        alert("졌다.");
+        computerScore.innerText = `${++comScore}`;
     }
         
 }
 
 function Re(){
-    randomImoge = setInterval(Random,1000);
+    randomImoge = setInterval(Random,100);
 }
