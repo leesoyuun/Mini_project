af1b4392a91bd1a884af751e75acfa4029e370e6
leesoyuun <leesoyun4206@naver.com>
2022-08-10 00:53:47 +0900

    Randomly print only one letter differently

--- Find anothor letter/index.js
+++ Find anothor letter/index.js
@@ -1,11 +1,12 @@
 let wordIn = document.getElementById("inner_text");
 let putword = wordIn.getElementsByTagName("td")
-
-
+let randomNum = Math.floor(Math.random() * 10)  + 6
+console.log(randomNum)
 for(let i = 0 ; i <putword.length ;i++){
-    putword[i].innerHTML = '뚧'
+    if(i == randomNum)
+        putword[i].innerHTML = '뜗';
+    else
+        putword[i].innerHTML = '뚧'
 }
-
-
 
 
