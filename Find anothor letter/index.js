c45906f10fc30a95625a5b2375f687d74be6a54c
leesoyuun <leesoyun4206@naver.com>
2022-08-11 23:11:34 +0900

    add EvenetListener

--- Find anothor letter/index.js
+++ Find anothor letter/index.js
@@ -1,12 +1,22 @@
 let wordIn = document.getElementById("inner_text");
-let putword = wordIn.getElementsByTagName("td")
-let randomNum = Math.floor(Math.random() * 10)  + 6
-console.log(randomNum)
-for(let i = 0 ; i <putword.length ;i++){
-    if(i == randomNum)
-        putword[i].innerHTML = '뜗';
-    else
-        putword[i].innerHTML = '뚧'
-}
+let putword = wordIn.getElementsByTagName("td");
+let randomNum = Math.floor(Math.random() * 10)  + 6;
 
+
+
+
+for(let i = 0 ; i <putword.length ;i++){
+    if(i == randomNum){
+        putword[i].innerHTML = '뜗';
+        putword[i].addEventListener('click',()=>{
+            alert('👍성공')
+        })
+    }
+    else{
+        putword[i].innerHTML = '뚧'
+        putword[i].addEventListener('click',()=>{
+            alert('❌')
+        })
+    }
+}
 
