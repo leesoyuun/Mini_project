d7b8764482b32c2eb4bdc4e6d9143131236187c7
leesoyuun <leesoyun4206@naver.com>
2022-08-14 00:44:38 +0900

    add timer

--- Find anothor letter/index.js
+++ Find anothor letter/index.js
@@ -1,8 +1,18 @@
 let wordIn = document.getElementById("inner_text");
 let putword = wordIn.getElementsByTagName("td");
 let randomNum = Math.floor(Math.random() * 10)  + 6;
+let time = document.getElementsByClassName("time")[0];
+let sec = "15"
 
+let x = setInterval(function(){
+    time.innerHTML = sec + "초";
+    sec--;
 
+    if(sec<0){
+        clearInterval(x);
+        time.innerHTML = "시간초과!!";
+}
+},1000)
 for(let i = 0 ; i <putword.length ;i++){
     if(i == randomNum){
         putword[i].innerHTML = '뜗';
